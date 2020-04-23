2b736b5b6e440c697b53ffdb448bd91967faeaf2
In1ight <55357211+In1ight@users.noreply.github.com>
2020-04-23 22:12:44 +0500

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, redirect, flash, url_for
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Roma'}
+    posts = [
+        {
+            'author': {'username': 'John', 'id': 1},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan', 'id': 2},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит', 'id': 3},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
