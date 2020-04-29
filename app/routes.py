dd57e927faf79e0b00348f7f2bde8034438e95c2
In1ight <55357211+In1ight@users.noreply.github.com>
2020-04-29 16:05:02 +0500

    Chapter 8 followers
    Конец 8 главы фолловеров

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,37 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import Post, User
 
 
+@app.route('/follow/<username>')
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot follow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash('You are following {}'.format(username))
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are unfollowing {}'.format(username))
+    return redirect(url_for('user', username=username))
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
