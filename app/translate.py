5d6f22f11c20ea22989494d4011532b5c61e09ed
In1ight <55357211+In1ight@users.noreply.github.com>
2020-05-11 21:59:15 +0500

    Better structure of app

--- app/translate.py
+++ app/translate.py
@@ -1,17 +1,17 @@
 import json
 import requests
 from flask_babel import _
-from app import app
+from flask import current_app
 
 
 def translate(text, source_language, dest_language):
-    if 'YA_KEY' not in app.config or \
-            not app.config['YA_KEY']:
+    if 'YA_KEY' not in current_app.config or \
+            not current_app.config['YA_KEY']:
         return _('Error: the translation service is not configured.')
-    auth = {'Ocp-Apim-Subscription-Key': app.config['YA_KEY']}
+    auth = {'Ocp-Apim-Subscription-Key': current_app.config['YA_KEY']}
     r = requests.get('https://translate.yandex.net/api/v1.5/tr.json'
                      '/translate?key={}&text={}&lang={}-{}'.format(
-                         app.config['YA_KEY'], text, source_language, dest_language), headers=auth)
+                         current_app.config['YA_KEY'], text, source_language, dest_language), headers=auth)
    # print(r.content[0])
     if r.status_code != 200:
         return _('Error: the translation service failed.')
